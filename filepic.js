5e7c16c62ab87b94ba76e3a0f444c1f3f3d71595
shkkaise <123665233+shkkaise@users.noreply.github.com>
2023-11-16 13:02:53 +0530

    Update filepic.js

--- filepic.js
+++ filepic.js
@@ -1,6 +1,6 @@
 
 var randomNumber = Math.floor(Math.random() * 10) + 1;
-var randomImage = "KAIS00" + randomNumber + ".JPG";
+var randomImage = "KAIS00" + randomNumber + ".webp";
 var imgSrc = "images/" + randomImage;
 var image1 = document.querySelectorAll("img")[0]
 image1.setAttribute("src", imgSrc);
